4475431849efd49845374d8bfeb8c8eaeaeb17e7
AVINASH MAURYA <121997937+avmaurya07@users.noreply.github.com>
2024-12-06 19:08:32 +0530

    Delete src directory

--- src/api/mak/services/mak.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * mak service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::mak.mak');
--- src/api/patient/services/patient.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * patient service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::patient.patient');
